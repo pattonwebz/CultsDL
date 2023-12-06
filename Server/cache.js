98dc8300e7d4f9b1b1759515f0e67487ab54812e
William Patton <will@pattonwebz.com>
2023-12-06 04:47:24 +0000

    Lint: fix various style rules in cache file

--- Server/cache.js
+++ Server/cache.js
@@ -1,11 +1,11 @@
-const fileSystemCache = require("file-system-cache");
-const {CONSTANTS} = require("./constants");
-const {CACHE_DIR} = CONSTANTS;
+const fileSystemCache = require('file-system-cache');
+const { CONSTANTS } = require('./constants');
+const { CACHE_DIR } = CONSTANTS;
 
 const cache = fileSystemCache.default({
-    basePath: CACHE_DIR,  // (optional) Path where cache files are stored (default).
-    hash: "sha1",          // (optional) A hashing algorithm used within the cache key.
-    ttl: 60 * 60   // (optional) A time-to-live (in secs) on how long an item remains cached.
+	basePath: CACHE_DIR, // (optional) Path where cache files are stored (default).
+	hash: 'sha1', // (optional) A hashing algorithm used within the cache key.
+	ttl: 60 * 60 // (optional) A time-to-live (in secs) on how long an item remains cached.
 });
 
 module.exports = cache;
