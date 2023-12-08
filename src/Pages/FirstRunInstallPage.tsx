a0260849de103dc2010babf129944b309b9f10f4
pattonwebz <will@pattonwebz.com>
2023-12-08 19:51:10 +0000

    Fix some first install page issues with buttons showing due to lack of context access

--- src/Pages/FirstRunInstallPage.tsx
+++ src/Pages/FirstRunInstallPage.tsx
@@ -1,38 +1,38 @@
 // src/Components/FirstRunMessage.tsx
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography, Button, Box, ButtonGroup } from '@material-ui/core';
-import SessionTokenInput from "../Componets/SessionTokenInput";
-import {Dialog} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import SessionTokenInput from '../Componets/SessionTokenInput';
+import { Dialog } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useUserData } from '../Contexts/UserDataContext';
 
 const ipcRenderer = window.electron.ipcRenderer;
 
-const FirstRunMessage: React.FC = () => {
+const FirstRunInstallPage: React.FC = () => {
+	const [openModal, setOpenModal] = useState(false);
+	const [showFinishButton, setShowFinishButton] = useState(false);
 
-	const [openModal, setOpenModal] = React.useState(false);
-	const [showFinishButton, setShowFinishButton] = React.useState(false);
-
-	const [installed, setInstalled] = React.useState(true);
+	const {installed, setInstalled} = useUserData();
 
 	const navigate = useNavigate();
 
 	const handleFinished = () => {
-		localStorage.setItem("installComplete", "true");
+		localStorage.setItem('installComplete', 'true');
 		setInstalled(true);
 		navigate('/main');
-	}
+	};
 
- const handleInstallClick = () => {
-  ipcRenderer.invoke('install');
- };
+	const handleInstallClick = () => {
+		ipcRenderer.invoke('install');
+	};
 
- const handleSetTokenClick = () => {
-  setOpenModal(true);
- }
+	const handleSetTokenClick = () => {
+		setOpenModal(true);
+	};
 
- const handleModalClose = () => {
-	   setOpenModal(false);
- }
+	const handleModalClose = () => {
+		setOpenModal(false);
+	};
 
 	useEffect(() => {
 		ipcRenderer.on('install-reply', (event, data) => {
@@ -50,35 +50,38 @@ const FirstRunMessage: React.FC = () => {
 		});
 	}, []);
 
- return (
-  <>
-   <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
-    <Typography variant="h2" component="h1" gutterBottom>
-     First Run Install
-    </Typography>
-    <Typography variant="h5" component="h2" gutterBottom>
-     This is your first time running the application
-    </Typography>
-    <Typography variant="body1" gutterBottom>
-     This tool needs to setup some directories and files to store it's data before it can be used.
-    </Typography>
-    <Typography variant="body1" gutterBottom>
-     After that you need to provide a session token from a logged in session on cults.
-    </Typography>
-    <ButtonGroup orientation="vertical" aria-label="vertical outlined primary button group">
-	   {showFinishButton || installed ? (
-		   <Button variant="contained" color="primary" size="large" onClick={handleFinished}>
-			   Finish
-		   </Button>
-	   ) : (
-		   <><Button variant="contained" color="primary" size="large" onClick={handleInstallClick}>
-			   Create Data Directories
-		   </Button><Button variant="outlined" color="primary" size="large" onClick={handleSetTokenClick}>
-			   Set Session Token
-		   </Button></>
-	   )}
-    </ButtonGroup >
-   </Box>
+	return (
+		<>
+			<Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
+				<Typography variant="h2" component="h1" gutterBottom>
+					First Run Install
+				</Typography>
+				<Typography variant="h5" component="h2" gutterBottom>
+					This is your first time running the application
+				</Typography>
+				<Typography variant="body1" gutterBottom>
+					This tool needs to setup some directories and files to store it's data before it can be used.
+				</Typography>
+				<Typography variant="body1" gutterBottom>
+					After that you need to provide a session token from a logged in session on cults.
+				</Typography>
+				<ButtonGroup orientation="vertical" aria-label="vertical outlined primary button group">
+					{showFinishButton || installed
+						? (
+							<Button variant="contained" color="primary" size="large" onClick={handleFinished}>
+								Finish
+							</Button>
+						)
+						: (
+							<><Button variant="contained" color="primary" size="large" onClick={handleInstallClick}>
+								Create Data Directories
+							</Button><Button variant="outlined" color="primary" size="large" onClick={handleSetTokenClick}>
+								Set Session Token
+							</Button></>
+						)
+					}
+				</ButtonGroup >
+			</Box>
 	  <Dialog
 		  open={openModal}
 		  onClose={handleModalClose}
@@ -89,8 +92,8 @@ const FirstRunMessage: React.FC = () => {
 			  <SessionTokenInput/>
 		  </Box>
 	  </Dialog>
-  </>
- );
+		</>
+	);
 };
 
-export default FirstRunMessage;
+export default FirstRunInstallPage;
